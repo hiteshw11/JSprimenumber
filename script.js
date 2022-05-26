d5c8a53cb08eb08e1313944972e656f2e6904ced
Hitesh Wadhwa <50346971+hiteshw11@users.noreply.github.com>
2022-05-27 01:19:19 +0530

    Create script.js

---
+++ script.js
@@ -0,0 +1,7 @@
+const num = Number(prompt(('Enter Your Number')));
+if (num % 2 == 0) {
+    console.log('The Number is a prime number');
+}
+else {
+    console.log('Number is a non-prime number');
+}
