f5ceddada4e1c41c9d805d1541725bdd60f958c5
Hitesh Wadhwa <50346971+hiteshw11@users.noreply.github.com>
2022-05-28 19:17:35 +0530

    Using Console.log twice, once in function and once with return variable brings undefined error along with actual output

---
+++ UndefinedErrorCode.js
@@ -0,0 +1,6 @@
+function Two(a, b) {
+    console.log(a);
+}
+
+let c = Two(1, 2);
+console.log(c); // if you print console log this one more time after console logging in function you will get undefined in the output along with actual output
