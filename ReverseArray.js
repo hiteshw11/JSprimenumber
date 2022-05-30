72fbfa79184df3af42295c48296fca038cbb4703
Hitesh Wadhwa <50346971+hiteshw11@users.noreply.github.com>
2022-05-30 12:37:03 +0530

    Print An Array In Reverse Order

---
+++ ReverseArray.js
@@ -0,0 +1,24 @@
+let arrOne = [
+    'Jonas',
+    'Smith',
+    'Portugal',
+    1991,
+    'teacher'
+]
+
+for (let i = arrOne.length - 1; i >= 0; i--) {
+
+    console.log(arrOne[i]);
+
+
+}
+
+/* Output
+
+teacher
+1991
+Portugal
+Smith
+Jonas
+
+*/
