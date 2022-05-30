e77a24c291e2bf0814dfdfbaeeb9a5cb0140a582
Hitesh Wadhwa <50346971+hiteshw11@users.noreply.github.com>
2022-05-30 15:34:16 +0530

    Loop Ends When Random Number Comes As 3

---
+++ WhileLoopRandomNum.js
@@ -0,0 +1,16 @@
+let num = Math.trunc(Math.random() * 6) + 1;
+
+while (num !== 3) {
+    num = Math.trunc(Math.random() * 6) + 1;
+    console.log(`The Number rolled is ${num}`);
+}
+
+/*
+
+The Number rolled is 5
+The Number rolled is 2
+The Number rolled is 6
+The Number rolled is 1
+The Number rolled is 3
+
+*/
