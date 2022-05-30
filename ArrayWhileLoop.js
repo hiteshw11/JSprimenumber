2555d5406e5f584af34dba9baa7ea5eeda6aeacd
Hitesh Wadhwa <50346971+hiteshw11@users.noreply.github.com>
2022-05-30 16:08:38 +0530

    Use While Loop To Perform Calculation From An Array

--- ArrayWhileLoop.js
+++ ArrayWhileLoop.js
@@ -1,19 +1,23 @@
 let populations = [6, 120, 360, 35]
+let percentPop = [];
 let num = 0;
 worldPop = 7900;
+
 function percentageOfWorld2(population, worldPop) {
     return (population / worldPop) * 100;
 }
 
 while (populations.length !== num) {
-    console.log(percentageOfWorld2(populations[num], worldPop));
+    let num2 = (percentageOfWorld2(populations[num], worldPop));
+    percentPop.push(num2);
     num++;
 }
 
+console.log(percentPop);
+
 /* Output
 
-0.0759493670886076
-1.5189873417721518
-4.556962025316456
-0.44303797468354433
+(4) [0.0759493670886076, 1.5189873417721518, 4.556962025316456, 0.44303797468354433]
+
+
 */
