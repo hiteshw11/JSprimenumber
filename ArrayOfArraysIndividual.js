3aad9c8b54fd628c49148eaf83b87eda02000e5b
Hitesh Wadhwa <50346971+hiteshw11@users.noreply.github.com>
2022-05-30 14:11:01 +0530

    Print Array Of Arrays Element By Element

---
+++ ArrayOfArraysIndividual.js
@@ -0,0 +1,23 @@
+const listOfNeighbours = [
+    ['Canada', 'Mexico'],
+    ['Spain'],
+    ['Norway', 'Sweden', 'Russia'],
+];
+
+for (let i = 0; i < listOfNeighbours.length; i++) {
+    for (let j = 0; j < listOfNeighbours[i].length; j++) {
+        console.log(listOfNeighbours[i][j]);
+    }
+
+}
+
+/* Output
+
+Canada
+Mexico
+Spain
+Norway
+Sweden
+Russia
+
+*/
