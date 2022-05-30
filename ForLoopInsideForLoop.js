a024ca8f6179053403ff2a9c2d4f2fdbc71ade58
Hitesh Wadhwa <50346971+hiteshw11@users.noreply.github.com>
2022-05-30 12:33:05 +0530

    Create A Nested For Loop for Each Exercise And Each Rep

--- ForLoopInsideForLoop.js
+++ ForLoopInsideForLoop.js
@@ -8,3 +8,20 @@ for (let exercise = 1; exercise <= exercisetotal; exercise++) {
         console.log(`This is rep number ${rep} for exercise number ${exercise}`);
     }
 }
+
+/* Output
+Enter The Total Number Of Exercises
+2
+
+Enter the reps for each exercise
+3
+
+-----This is exercise number 1-----
+This is rep number 1 for exercise number 1
+This is rep number 2 for exercise number 1
+This is rep number 3 for exercise number 1
+-----This is exercise number 2-----
+This is rep number 1 for exercise number 2
+This is rep number 2 for exercise number 2
+This is rep number 3 for exercise number 2
+*/
